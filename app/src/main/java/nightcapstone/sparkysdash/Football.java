c08750e40fc1eb32477931fc607dc0f8f43d1b61
Matthew Vehon <mvehon@asu.edu>
2016-04-18 10:52:41 -0700

    Added hurdle type

--- app/src/main/java/nightcapstone/sparkysdash/Football.java
+++ app/src/main/java/nightcapstone/sparkysdash/Football.java
@@ -7,28 +7,31 @@ import java.util.Random;
 
 public class Football extends Sprite {
 
-    public static Bitmap globalBitmap;
-    public int lane;
+    public static Bitmap ball_img;
+    public static Bitmap hurdle_img;
 
     public Football(GameView view, Game game) {
         super(view, game);
-
-        //Randomly decide which 'lane' to position in
-        Random random = new Random();
-        lane = random.nextInt(4);
-        Log.d("Lane", Integer.toString(lane));
-
-        if (globalBitmap == null) {
-            //TODO set conditional here for lane=0 to be ground obstacle image
-            //TODO put animation handling here
-            globalBitmap = getScaledBitmapAlpha8(game, R.drawable.footballsheet2);
+        if (ball_img == null) {
+            ball_img = getScaledBitmapAlpha8(game, R.drawable.footballsheet2);
         }
-        this.bitmap = globalBitmap;
-        //this.width = this.bitmap.getWidth();
+        this.bitmap = ball_img;
+        //this.bitmap = globalBitmap;
         this.height = this.bitmap.getHeight();
+        this.width = this.bitmap.getWidth() / (colNr = 10);                                             // 10 frames in a row
+        this.frameTime = 1;                                                                             // the frame will change every 3 runs
+    }
 
-        this.width = this.bitmap.getWidth() / (colNr = 10);                                      // 10 frames in a row
-        this.frameTime = 1;                                                                     // the frame will change every 3 runs
+    public Football(GameView view, Game game, int ln) {
+        super(view, game);
+        if (hurdle_img == null) {
+            hurdle_img = getScaledBitmapAlpha8(game, R.drawable.hurdle2);
+        }
+        this.bitmap = hurdle_img;
+        //this.bitmap = globalBitmap;
+        this.height = this.bitmap.getHeight();
+        this.width = this.bitmap.getWidth() / (colNr = 1);
+        //this.frameTime = 100;                                                                     // the frame will change every 3 runs
     }
 
     //Sets the x and y coordinates
@@ -37,7 +40,7 @@ public class Football extends Sprite {
         this.y = y;
     }
 
-    public void move(){
+    public void move() {
         super.move();
     }
 
